194adf0d56ebcf519d39e880a19cc77c7056f602
Mekala-balaiah <balaiahyadav12@gmail.com>
2026-03-21 01:41:49 +0530

    Restrict CORS to specific origin with credentials

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const analyticsRoutes = require("./routes/analyticsRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://microbizcopilot.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
